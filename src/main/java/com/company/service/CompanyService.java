050f8df7620474b3b26c2178d32ca593e37658d5
SamanehPirveisi <samaneh_pirveisy@yahoo.com>
2018-06-07 19:00:39 +0200

    adding companiDao methods to companyService

--- src/main/java/com/company/service/CompanyService.java
+++ src/main/java/com/company/service/CompanyService.java
@@ -1,8 +1,35 @@
 package com.company.service;
 
+import java.util.List;
+
 import com.company.dao.CompanyDao;
+import com.company.model.Company;
+import com.company.model.User;
 
 public class CompanyService {
-	private CompanyDao companyDao=new CompanyDao();
+	private CompanyDao companyDao = new CompanyDao();
 
+	public boolean createCompany(Company c) {
+		return companyDao.createCompany(c);
+	}
+
+	public Company getCompanyById(long id) {
+		return companyDao.getCompanyById(id);
+	}
+
+	public boolean updateCompany(Company c) {
+		return companyDao.updateCompany(c);
+	}
+
+	public boolean updateCompanyById(long id, String name) {
+		return companyDao.updateCompanyById(id, name);
+	}
+
+	public boolean deleteCompany(Company c) {
+		return companyDao.deleteCompany(c);
+	}
+
+	public List<User> getListOfUserForCompany(long id) {
+		return companyDao.getListOfUserForCompany(id);
+	}
 }
