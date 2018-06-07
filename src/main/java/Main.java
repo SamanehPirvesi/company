208f3078682ecb6cc0482efcc84e803c22230024
SamanehPirveisi <samaneh_pirveisy@yahoo.com>
2018-06-07 21:32:35 +0200

    adding someexample for testing server class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,9 @@ import com.company.model.Address;
 import com.company.model.Company;
 import com.company.model.User;
 import com.company.model.WorkingDay;
+import com.company.service.CompanyService;
+import com.company.service.UserService;
+import com.company.service.WorkingDayService;
 
 import utility.DateUtil;
 
@@ -19,27 +22,33 @@ public class Main {
 		Address a1 = new Address("torino", "raconigi", "34", "3456");
 		Address a2 = new Address("torino", "netro", "8", "35671");
 		Address a3 = new Address("torino", "pastrengo", "14", "45345");
+		Address a4 = new Address("torino", "lione", "45/b", "13467");
 		User u1 = new User("sara", "ahmadi", "saraahmadi", "s1234", "n54678", a1, "011-3456", "38-3245", true, 1200.6);
 		User u2 = new User("zoya", "azizi", "zazizi", "z657", "n9876", a2, "011-2356", "39-65758", true, 15.6);
 		User u3 = new User("marco", "toserani", "mtoserani", "m786", "n56667", a3, "011-23566754", "39-76443", true,
 				13.70);
+		User u4 = new User("sara", "sansone", "srsn", "s456", "n6783", a4, "011-276543", "38-43256", true,
+				2000.00);
 		Date d1 = new Date(12 / 07 / 1980);
 		Date d2 = DateUtil.getStringToDate("07/07/2018");
 		Date d3 = DateUtil.getStringToDate("25/08/1960");
 		Date d4 = DateUtil.getStringToDate("05/06/2018");
 		Date d5 = DateUtil.getStringToDate("13/06/1984");
 		Date d6 = DateUtil.getStringToDate("06/06/2018");
+		Date d7 = DateUtil.getStringToDate("24/11/1975");
 
-		UserDao uDao = new UserDao();
-		CompanyDao cDao = new CompanyDao();
-		WorkingDayDao wdDao = new WorkingDayDao();
+		UserService us = new UserService();
+		CompanyService cs = new CompanyService();
+		WorkingDayService wds = new WorkingDayService();
 		WorkingDay wd1 = new WorkingDay();
 		WorkingDay wd2 = new WorkingDay();
 		WorkingDay wd3 = new WorkingDay();
+		WorkingDay wd4 = new WorkingDay();
 
 		u1.setBirthday(d1);
 		u2.setBirthday(d3);
 		u3.setBirthday(d5);
+		u4.setBirthday(d7);
 
 		c1.addUsers(u1);
 		u1.setCompany(c1);
@@ -48,6 +57,7 @@ public class Main {
 		u2.setCompany(c1);
 
 		c1.addUsers(u3);
+			
 
 		LocalTime t1 = LocalTime.of(8, 00);
 		LocalTime t2 = LocalTime.of(18, 00);
@@ -73,41 +83,47 @@ public class Main {
 		wd3.setFinishTime(t6);
 		wd3.setUser(u3);
 		u3.addWorkingDays(wd3);
+		
+		wd4.setDate(d2);
+		wd4.setInitTime(t1);
+		wd4.setFinishTime(t4);
+		u4.addWorkingDays(wd4);
+		
 
-		User user = uDao.getUserById(4);
-		wd3.setUser(user);
-		// wdDao.createWorkingDay(wd3);
+		User user = us.getUserById(21);
+//		wd4.setUser(user);
+//		 wds.createWorkingDay(wd4);
 		//
-		// Company company=cDao.getCompanyById(1);
-		// u3.setCompany(company);
+//		 Company company=cs.getCompanyById(1);
+//		 user.setCompany(company);
+//		 company.addUsers(user);
+//		 us.updateUser(user);
+//		 cs.updateCompany(company);
+//		 u4.setCompany(company);
 		// double f=DateUtil.calculateHours(t6, t5);
 		//
 		// System.out.println(f);
-		// // wdDao.createWorkingDay(wd1);
-		// uDao.createUser(u3);
-		// cDao.createCompany(c1);
-		// cDao.createCompany(c1);
-		// User user=uDao.getUserByUserName("saraahmadi");
-		// System.out.println(user.getUserName()+user.getMobilenumber());
-		// uDao.getAllUser().stream().map(m->m.getSurname()).forEach(System.out::println);
-		// System.out.println(uDao.isUserActive(2));
-		// uDao.updatePassworldByUsername("zazizi", "a7869");
-		// uDao.deleteUserById(6);
-		// uDao.activeExsistentUserByUsername("saraahmadi");
-		// uDao.deactiveExsistentUserByUsername("saraahmadi");
-		// cDao.getListOfUserForCompany(1).stream().map(m->m.getSurname()).forEach(System.out::println);
-		// User u=uDao.getUserById(6);
-		// uDao.deleteUser(u);
-		// WorkingDay w=wdDao.getWorkingDaywdById(14);
-		// wdDao.deleteWorkingDay(w);
-//		 System.out.println(wdDao.GetHoursOfUserByMonth(4,6));
-		// System.out.println(wdDao.getDaysHolidayForUserByMonth(4,6));
-//		 System.out.println(wdDao.GetPermissionHoursForByMonth(4, 6));
-//		 System.out.println(wdDao.GetPermissionHoursForByYear(4, 2018));
-		// System.out.println(wdDao.getDaysHolidayForUserByYear(4, 2018));
-//		WorkingDay workingday = wdDao.getWorkingDaywdById(16);
+//			us.createUser(u4);
+		// cs.createCompany(c1);
+//		 User user1=us.getUserByUserName("saraahmadi");
+//		 System.out.println(user1.getUserName()+user1.getMobilenumber());
+//		 us.getAllUser().stream().map(m->m.getSurname()).forEach(System.out::println);
+//		 System.out.println(us.isUserActive(2));
+//		 us.updatePassworldByUsername("zazizi", "b675");
+//		 us.activeExsistentUserByUsername("saraahmadi");
+//		 us.deactiveExsistentUserByUsername("saraahmadi");
+//		 cs.getListOfUserForCompany(1).stream().map(m->m.getSurname()).forEach(System.out::println);
+//		 us.deleteUser(user);
+//		 WorkingDay w=wds.getWorkingDaywdById(22);
+//		 wds.deleteWorkingDay(w);
+//		 System.out.println(wds.GetHoursOfUserByMonth(4,6));
+//		 System.out.println(wds.getDaysHolidayForUserByMonth(4,6));
+//		 System.out.println(wds.GetPermissionHoursForUserByMonth(4, 6));
+//		 System.out.println(wds.GetPermissionHoursForUserByYear(4, 2018));
+//		 System.out.println(wds.getDaysHolidayForUserByYear(4, 2018));
+//		WorkingDay workingday = wds.getWorkingDaywdById(16);
 //		workingday.setHoursPermission(t7);
-//		wdDao.updateWorkingDay(workingday);
+//		wds.updateWorkingDay(workingday);
 		//
 	}
 
--- src/main/java/com/company/service/WorkingDayService.java
+++ src/main/java/com/company/service/WorkingDayService.java
@@ -4,31 +4,40 @@ import com.company.dao.WorkingDayDao;
 import com.company.model.WorkingDay;
 
 public class WorkingDayService {
-	private WorkingDayDao workingDayDao=new WorkingDayDao();
+	private WorkingDayDao workingDayDao = new WorkingDayDao();
+
 	public boolean createWorkingDay(WorkingDay wd) {
 		return workingDayDao.createWorkingDay(wd);
 	}
+
 	public WorkingDay getWorkingDaywdById(long id) {
 		return workingDayDao.getWorkingDaywdById(id);
-			}
+	}
+
 	public boolean updateWorkingDay(WorkingDay wd) {
 		return workingDayDao.updateWorkingDay(wd);
 	}
+
 	public int GetHoursOfUserByMonth(long id, int month) {
 		return workingDayDao.GetHoursOfUserByMonth(id, month);
 	}
+
 	public int getDaysHolidayForUserByMonth(long id, int month) {
 		return workingDayDao.getDaysHolidayForUserByMonth(id, month);
 	}
+
 	public int getDaysHolidayForUserByYear(long id, int year) {
 		return workingDayDao.getDaysHolidayForUserByYear(id, year);
 	}
+
 	public int GetPermissionHoursForUserByMonth(long id, int month) {
 		return workingDayDao.GetPermissionHoursForUserByMonth(id, month);
 	}
+
 	public int GetPermissionHoursForUserByYear(long id, int year) {
-		return workingDayDao.GetPermissionHoursForUserByYear(id, year);	
+		return workingDayDao.GetPermissionHoursForUserByYear(id, year);
 	}
+
 	public boolean deleteWorkingDay(WorkingDay wd) {
 		return workingDayDao.deleteWorkingDay(wd);
 	}
